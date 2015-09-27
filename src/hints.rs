499f931a89c09eacbaaf6bdf66f49b31ffee848d
Hendrik Sollich <hendrik@hoodie.de>
2015-09-27 19:42:49 +0200

    fixed: hint types other than just strings

--- src/hints.rs
+++ src/hints.rs
@@ -34,44 +34,82 @@ pub enum NotificationHint
     Transient(bool),
     X(i32),
     Y(i32),
+    /// Pass me a NotificationUrgency, either Low, Normal or Critical
     Urgency(NotificationUrgency),
     Custom(String,String),
-    Invalid // TODO remove me
+    Invalid // TODO find a better solution to this
+}
+
+impl NotificationHint
+{
+    pub fn as_bool(&self) -> Option<bool>
+    {
+        match self
+        {
+            &NotificationHint::ActionIcons(inner)   => Some(inner),
+            &NotificationHint::Resident(inner)      => Some(inner),
+            &NotificationHint::SuppressSound(inner) => Some(inner),
+            &NotificationHint::Transient(inner)     => Some(inner),
+            _ => None
+        }
+    }
+    pub fn as_i32(&self) -> Option<i32>
+    {
+        match self
+        {
+            &NotificationHint::X(inner) => Some(inner),
+            &NotificationHint::Y(inner) => Some(inner),
+            _ => None
+        }
+    }
+
+    pub fn as_str(&self) -> Option<&str>
+    {
+        match self
+        {
+            &NotificationHint::DesktopEntry(ref inner) => Some(&inner),
+            &NotificationHint::ImagePath(ref inner) => Some(&inner),
+            &NotificationHint::SoundFile(ref inner) => Some(&inner),
+            &NotificationHint::SoundName(ref inner) => Some(&inner),
+            _ => None
+        }
+
+    }
 }
 
 impl<'a> From<&'a NotificationHint> for MessageItem
 {
     fn from(hint: &NotificationHint) -> MessageItem
     {
-        let hint:(String,String) = match hint {
-            &NotificationHint::ActionIcons(ref value)   => (ACTION_ICONS   .to_owned(), format!("{}",  value)), // bool
-            &NotificationHint::Category(ref value)      => (CATEGORY       .to_owned(), value.clone()),
-            &NotificationHint::DesktopEntry(ref value)  => (DESKTOP_ENTRY  .to_owned(), value.clone()),
-          //&NotificationHint::ImageData(iiibiiay)      => (IMAGE_DATA     .to_owned(), format!("{:?}",  value)),
-            &NotificationHint::ImagePath(ref value)     => (IMAGE_PATH     .to_owned(), value.clone()),
-          //&NotificationHint::IconData(iiibiiay)       => (ICON_DATA      .to_owned(), format!("{:?}",  value)),
-            &NotificationHint::Resident(ref value)      => (RESIDENT       .to_owned(), format!("{}",  value)), // bool
-            &NotificationHint::SoundFile(ref value)     => (SOUND_FILE     .to_owned(), value.clone()),
-            &NotificationHint::SoundName(ref value)     => (SOUND_NAME     .to_owned(), value.clone()),
-            &NotificationHint::SuppressSound(value)     => (SUPPRESS_SOUND .to_owned(), format!("{}",  value)),
-            &NotificationHint::Transient(value)         => (TRANSIENT      .to_owned(), format!("{}",  value)),
-            &NotificationHint::X(value)                 => (X              .to_owned(), format!("{}",  value)),
-            &NotificationHint::Y(value)                 => (Y              .to_owned(), format!("{}",  value)),
-            &NotificationHint::Urgency(value)           => (URGENCY        .to_owned(), format!("{}",  value as u32)),
-            &NotificationHint::Custom(ref key, ref val) => (key            .to_owned(), val.to_owned ()),
-            &NotificationHint::Invalid                  => ("invalid"      .to_owned(), "Invalid".to_owned ())
+        let hint:(String,MessageItem) = match hint {
+            &NotificationHint::ActionIcons(value)   => (ACTION_ICONS   .to_owned(), MessageItem::Bool(value)), // bool
+            &NotificationHint::Category(ref value)      => (CATEGORY       .to_owned(), MessageItem::Str(value.clone())),
+            &NotificationHint::DesktopEntry(ref value)  => (DESKTOP_ENTRY  .to_owned(), MessageItem::Str(value.clone())),
+          //&NotificationHint::ImageData(iiibiiay)      => (IMAGE_DATA     .to_owned(), MessageItem::Str(format!("{:?}",  value)),
+            &NotificationHint::ImagePath(ref value)     => (IMAGE_PATH     .to_owned(), MessageItem::Str(value.clone())),
+          //&NotificationHint::IconData(iiibiiay)       => (ICON_DATA      .to_owned(), MessageItem::Str(format!("{:?}",  value)),
+            &NotificationHint::Resident(value)      => (RESIDENT       .to_owned(), MessageItem::Bool(value)), // bool
+            &NotificationHint::SoundFile(ref value)     => (SOUND_FILE     .to_owned(), MessageItem::Str(value.clone())),
+            &NotificationHint::SoundName(ref value)     => (SOUND_NAME     .to_owned(), MessageItem::Str(value.clone())),
+            &NotificationHint::SuppressSound(value)     => (SUPPRESS_SOUND .to_owned(), MessageItem::Bool(value)),
+            &NotificationHint::Transient(value)         => (TRANSIENT      .to_owned(), MessageItem::Bool(value)),
+            &NotificationHint::X(value)                 => (X              .to_owned(), MessageItem::Int32(value)),
+            &NotificationHint::Y(value)                 => (Y              .to_owned(), MessageItem::Int32(value)),
+            &NotificationHint::Urgency(value)           => (URGENCY        .to_owned(), MessageItem::Byte(value as u8)),
+            &NotificationHint::Custom(ref key, ref val) => (key            .to_owned(), MessageItem::Str(val.to_owned ())),
+            &NotificationHint::Invalid                  => ("invalid"      .to_owned(), MessageItem::Str("Invalid".to_owned()))
         };
 
         MessageItem::DictEntry(
             Box::new(hint.0.into()),
-            Box::new(MessageItem::Variant( Box::new(hint.1.into()) ))
+            Box::new(MessageItem::Variant( Box::new(hint.1) ))
             )
     }
 }
 
 //impl<'a> FromMessageItem<'a> for NotificationHint {
-impl<'a> From<&'a MessageItem> for NotificationHint {
-
+impl<'a> From<&'a MessageItem> for NotificationHint
+{
     fn from (item: &MessageItem) -> NotificationHint
     {
         match item{
@@ -89,8 +127,8 @@ impl<'a> From<&'a MessageItem> for NotificationHint {
             &MessageItem::DictEntry(ref key, ref value) if unwrap_message_str(&**key) == URGENCY        => NotificationHint::Urgency(
                 match unwrap_message_int(&**value){
                     0 => NotificationUrgency::Low,
-                    2 => NotificationUrgency::High,
-                    _ => NotificationUrgency::Medium
+                    2 => NotificationUrgency::Critical,
+                    _ => NotificationUrgency::Normal
                 }),
             &MessageItem::DictEntry(ref key, ref value) => NotificationHint::Custom(unwrap_message_str(&**key), unwrap_message_str(&**value)),
             foo @ _ => {println!("Invalid {:#?} ", foo); NotificationHint::Invalid}
@@ -102,18 +140,41 @@ impl<'a> From<&'a MessageItem> for NotificationHint {
 #[cfg(test)]
 mod test{
     use super::*;
+    use super::NotificationHint as Hint;
+    use NotificationUrgency::*;
     use dbus::*;
+    use dbus::MessageItem as Item;
 
     #[test]
     fn hint_to_item()
+    {
+        let category = &Hint::Category("testme".to_owned());
+        let item:Item= category.into();
+        let test_item= Item::DictEntry(
+            Box::new(Item::Str("category".into())),
+            Box::new(Item::Variant(  Box::new(Item::Str("testme".into()))  ))
+            );
+        assert_eq!(item, test_item);
+    }
+
+    #[test]
+    fn urgency()
+    {
+        let low = &Hint::Urgency(Low);
+        let low_item:Item= low.into();
+        let test_item= Item::DictEntry(
+            Box::new(Item::Str("urgency".into())),
+            Box::new(Item::Variant(  Box::new(Item::Byte(0)))  ));
+        assert_eq!(low_item, test_item);
+    }
+
+    #[test]
+    fn simple_hint_to_item()
     {
         let old_hint = &NotificationHint::Custom("foo".into(), "bar".into());
         let item:MessageItem = old_hint.into();
         let item_ref = &item;
         let hint:NotificationHint = item_ref.into();
         assert!(old_hint == &hint);
-        println!("old_hint: {:?}", old_hint);
-        println!("hint: {:?}", hint);
-        println!("item: {:?}", item);
     }
 }
