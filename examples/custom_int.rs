1722d8d3126a69c47b95036b45caa4d9fb7cbb41
Hendrik Sollich <hendrik@hoodie.de>
2016-02-28 01:09:48 +0100

    added examples for custom_int and reuse of notifications

---
+++ examples/custom_int.rs
@@ -0,0 +1,41 @@
+extern crate notify_rust;
+
+use self::notify_rust::{
+    Notification,
+    NotificationHint,
+};
+
+use self::notify_rust::{
+    NotificationHandle,
+};
+
+enum Volume {
+    Muted,
+    Percent(u32),
+}
+
+fn main(){
+
+    let percent = Volume::Percent(30);
+
+    let icon = match percent {
+        Volume::Muted => "notification-audio-volume-muted",
+        Volume::Percent(x) if x == 0 => "notification-audio-volume-off",
+        Volume::Percent(x) if x < 33 => "notification-audio-volume-low",
+        Volume::Percent(x) if x < 67 => "notification-audio-volume-medium",
+        _ => "notification-audio-volume-high",
+    };
+
+    let value = match percent {
+        Volume::Muted => 0,
+        Volume::Percent(p) => p,
+    };
+
+    Notification::new()
+        .summary(" ")
+        .icon(icon)
+        .hint(NotificationHint::SoundName("audio-volume-change".to_string()))
+        .hint(NotificationHint::Custom("synchronous".to_string(), "volume".to_string()))
+        .hint(NotificationHint::CustomInt("value".to_string(), value as i32))
+        .show();
+}
---
+++ examples/reuse.rs
@@ -0,0 +1,20 @@
+extern crate notify_rust;
+use notify_rust::Notification;
+fn main() {
+
+    // here we create a notification and then reuse a second time
+
+    let mut notification = Notification::new()
+        .summary("News update")
+        .icon("computer")
+        .body("Something bad happened")
+        .finalize();
+
+    notification.show().unwrap();
+
+    std::thread::sleep(std::time::Duration::from_millis(1500));
+
+    notification.body("just kidding, nothing happened");
+    notification.show().unwrap();
+
+}
