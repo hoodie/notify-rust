47ab984d0541b7802b8a154117b621216ebf4f55
Hendrik Sollich <hendrik@hoodie.de>
2016-08-01 14:19:04 +0200

    example to show notification in bg thread, good enough to fix #18

--- examples/on_close.rs
+++ examples/on_close.rs
@@ -1,13 +1,28 @@
-#![allow(unused_must_use)]
 extern crate notify_rust;
+
+use std::io;
+use std::thread;
+
 use notify_rust::Notification;
-fn main()
-{
-    Notification::new()
-        .summary("Time is running out")
-        .body("This will go away.")
-        .icon("clock")
-        .show().unwrap()
-        .on_close(||{println!("closed")});
+
+fn wait_for_keypress() {
+    println!("halted until you hit the \"ANY\" key");
+    io::stdin().read_line(&mut String::new()).unwrap();
+}
+
+fn print(){
+    println!("notification was closed");
+}
+
+fn main() {
+    thread::spawn(||
+                  Notification::new()
+                  .summary("Time is running out")
+                  .body("This will go away.")
+                  .icon("clock")
+                  .show()
+                  .map(|handler| handler.on_close(|| print()))
+                 );
+    wait_for_keypress();
 }
 
