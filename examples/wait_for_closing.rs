6d9f8c07909fb279c60363bf738f123adedcd015
Daniel Lockyer <thisisdaniellockyer@gmail.com>
2017-03-17 00:07:32 +0000

    Use if-let instead of matching on single pattern

--- examples/wait_for_closing.rs
+++ examples/wait_for_closing.rs
@@ -9,10 +9,8 @@ fn main()
         .hint(NotificationHint::Transient(true))
         .body("I'll be gone soon enough.\nSorry for the inconvenience.")
         .show().unwrap()
-        .wait_for_action({|action|
-            match action {
-                "__closed" => {println!("the notification was closed")}, // here "__closed" is a hardcoded keyword
-                _ => ()
-            }
+        .wait_for_action({|action| if let "__closed" = action {
+            println!("the notification was closed")
+        }
         });
 }
