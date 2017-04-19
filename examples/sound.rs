e2460ba311e45adb40f6763678d358326f8f3b7f
h4llow3En <mail@felixdoering.com>
2017-04-19 16:31:16 +0200

    add sound example

---
+++ examples/sound.rs
@@ -0,0 +1,17 @@
+#![allow(unused_must_use)]
+extern crate notify_rust;
+use notify_rust::Notification;
+
+#[cfg(target_os = "macos")]
+static SOUND: &'static str = "Ping";
+
+#[cfg(all(unix, not(target_os = "macos")))]
+static SOUND: &'static str ="message-new-instant";
+
+
+fn main() {
+    Notification::new()
+        .summary("notification with sound")
+        .sound_name(SOUND)
+        .show();
+}
