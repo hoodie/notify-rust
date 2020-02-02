00edbc9a2e44db642d02abfcb6931f0e3bb77563
Hendrik Sollich <hendrik@hoodie.de>
2020-02-02 17:48:01 +0100

    fix: reexport NotificationHandle

--- examples/wait_for_closing.rs
+++ examples/wait_for_closing.rs
@@ -1,19 +1,22 @@
 #![allow(unused_imports)]
-use notify_rust::{Hint, Notification};
+// import `NotificationHandle` to test re-export
+use notify_rust::{Hint, Notification, NotificationHandle};
 
 fn main() {
     #[cfg(all(unix, not(target_os = "macos")))]
-    Notification::new()
-        .summary("Don't Mind me")
-        .hint(Hint::Transient(true))
-        .body("I'll be gone soon enough.\nSorry for the inconvenience.")
-        .show()
-        .unwrap()
-        .wait_for_action(|action| {
+    {
+        let handle: NotificationHandle = Notification::new()
+            .summary("Don't Mind me")
+            .hint(Hint::Transient(true))
+            .body("I'll be gone soon enough.\nSorry for the inconvenience.")
+            .show()
+            .unwrap();
+        handle.wait_for_action(|action| {
             if "__closed" == action {
                 println!("the notification was closed")
             }
         });
+    }
 
     #[cfg(target_os = "macos")]
     Notification::new()
