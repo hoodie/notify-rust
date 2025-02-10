4c56594c8d7da8341495650e7f0d690ffd958bca
Hendrik Sollich <hendrik@hoodie.de>
2025-02-10 12:08:35 +0100

    docs: remove use of .then() in example

--- examples/wait_for_closing_async.rs
+++ examples/wait_for_closing_async.rs
@@ -7,22 +7,20 @@ fn main() {
 
 fn main() {
     use notify_rust::CloseReason;
-    use zbus::export::futures_util::FutureExt;
 
     zbus::block_on(async {
-        notify_rust::Notification::new()
+        let handle = notify_rust::Notification::new()
             .summary("Don't Mind me")
             .hint(notify_rust::Hint::Transient(true))
             .body("I'll be gone soon enough.\nSorry for the inconvenience.")
             .show_async()
-            .then(|handle| async move {
-                match handle {
-                    Ok(handle) => handle.on_close(|reason: CloseReason| {
-                        println!("the notification was closed reason: {reason:?}")
-                    }),
-                    Err(error) => println!("failed to send notification {error}"),
-                }
-            })
             .await;
+
+        match handle {
+            Ok(handle) => handle.on_close(|reason: CloseReason| {
+                println!("the notification was closed reason: {reason:?}")
+            }),
+            Err(error) => println!("failed to send notification {error}"),
+        }
     })
 }
